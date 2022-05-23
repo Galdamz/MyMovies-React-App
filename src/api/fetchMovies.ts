5b696166a3a1c6d93faa13053e9c71a6bb3f971c
Galdamez <galdamz@protonmail.com>
2022-05-22 22:15:31 -0600

    Cors config added

--- src/api/fetchMovies.ts
+++ src/api/fetchMovies.ts
@@ -4,7 +4,7 @@ import NProgress from 'nprogress';
 NProgress.configure({ showSpinner: false });
 
 const fetchMovies = axios.create({
-    baseURL: '/api/',
+    baseURL: '/api',
     headers: {
     }
 });
@@ -27,28 +27,28 @@ fetchMovies.interceptors.response.use((config) => {
 
 
 export const getPopularMovies = async () => {
-    const { data } = await fetchMovies.get(`movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`);
+    const { data } = await fetchMovies.get(`/movie/popular?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1`);
     return data;
 }
 
 export const getCasting = async (movieID: String): Promise<any> => {
-    const { data } = await fetchMovies.get(`movie/${movieID}/credits?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1&known_for_department=Acting`);
+    const { data } = await fetchMovies.get(`/movie/${movieID}/credits?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&page=1&known_for_department=Acting`);
     return data;
 }
 
 export const getDetails = async (movieID: String): Promise<any> => {
-    const { data } = await fetchMovies.get(`movie/${movieID}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`);
+    const { data } = await fetchMovies.get(`/movie/${movieID}?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`);
     return data;
 }
 
 export const searchMovies = async (movieID: String): Promise<any> => {
     const encodedSearch = encodeURIComponent(movieID.toString());
-    const { data } = await fetchMovies.get(`search/movie/?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&query=${encodedSearch}`);
+    const { data } = await fetchMovies.get(`/search/movie/?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&query=${encodedSearch}`);
     return data;
 }
 
 export const getSimilarMovies = async (movieID: String): Promise<any> => {
-    const { data } = await fetchMovies.get(`movie/${movieID}/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`);
+    const { data } = await fetchMovies.get(`/movie/${movieID}/similar?api_key=${import.meta.env.VITE_API_KEY}&language=en-US`);
     return data;
 }
 export default fetchMovies;
